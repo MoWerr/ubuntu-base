24ca6feb43c266c2720d062850e32c29ee89b666
MoWer <mbaran2601@gmail.com>
2021-04-25 11:16:55 +0200

    Build script accepts tag value as an argument

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:20.04 --push --pull --build-arg UBUNTU_TAG=20.04 .
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:18.04 --push --pull --build-arg UBUNTU_TAG=18.04 .
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:latest --push --pull --build-arg UBUNTU_TAG=latest .
+set -x && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --build-arg UBUNTU_TAG="$1" .
