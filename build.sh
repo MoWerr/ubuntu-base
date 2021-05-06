00e873e305bd6219445299628c95bbf0f315b19b
MoWer <mbaran2601@gmail.com>
2021-05-06 22:13:51 +0200

    Added misssing 'push' option

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-set -x && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --build-arg UBUNTU_TAG="$1" .
+set -x && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" .
