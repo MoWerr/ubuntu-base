418e6f238a24e68e71c2eb5108b9f3957a178de7
MoWer <mbaran2601@gmail.com>
2021-05-08 18:00:31 +0200

    Temporarily disabled non-amd64 architectures

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x && docker buildx build --platform linux/amd64 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" --build-arg S6_ARCH=amd64 .
-set -x && docker buildx build --platform linux/arm64 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" --build-arg S6_ARCH=aarch64 .
-set -x && docker buildx build --platform linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" --build-arg S6_ARCH=armhf .
+#set -x && docker buildx build --platform linux/arm64 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" --build-arg S6_ARCH=aarch64 .
+#set -x && docker buildx build --platform linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" --build-arg S6_ARCH=armhf .
 
