c7e7e31f0d9009f4ef46cb13707e538fb8f016a4
MoWer <mbaran2601@gmail.com>
2021-05-18 21:03:37 +0200

    Adopted build script to new common config

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-set -x && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:"$1" --pull --push --build-arg UBUNTU_TAG="$1" .
+set -x && \
+  docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:latest --pull --push --build-arg UBUNTU_TAG=latest . && \
+  docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:20.04 --pull --push --build-arg UBUNTU_TAG=20.04 . && \
+  docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t mowerr/ubuntu-base:18.04 --pull --push --build-arg UBUNTU_TAG=18.04 .
 
